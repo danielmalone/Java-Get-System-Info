1c75d9873eed11312a7440e7255bb0df0f2b69a4
Daniel Malone <danieljmalone@gmail.com>
2017-08-12 21:23:10 -0500

    display system info

--- src/com/finepointmobile/Main.java
+++ src/com/finepointmobile/Main.java
@@ -3,6 +3,12 @@ package com.finepointmobile;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        int processors = Runtime.getRuntime().availableProcessors();
+        long totalMemory = Runtime.getRuntime().totalMemory();
+
+        System.out.println("Available processors: " + processors);
+        System.out.println("Total Memory:: " + totalMemory);
     }
 }
