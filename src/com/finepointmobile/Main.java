42c859bd6fb5963527e4b0e21af64f8204d9c249
Daniel Malone <danieljmalone@gmail.com>
2017-08-12 21:28:22 -0500

    display more changes

--- src/com/finepointmobile/Main.java
+++ src/com/finepointmobile/Main.java
@@ -1,5 +1,8 @@
 package com.finepointmobile;
 
+import java.io.File;
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,5 +13,13 @@ public class Main {
 
         System.out.println("Available processors: " + processors);
         System.out.println("Total Memory:: " + totalMemory);
+
+        File[] roots = File.listRoots();
+        for (File root : roots) {
+            System.out.println("File system root: " + root.getAbsolutePath());
+            System.out.println("Total space (bytes): " + root.getTotalSpace());
+            System.out.println("Free space (bytes): " + root.getFreeSpace());
+            System.out.println("Usable space (bytes): " + NumberFormat.getInstance().format(root.getUsableSpace()));
+        }
     }
 }
